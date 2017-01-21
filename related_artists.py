b2dd47a6f81e31e094a653dd031cb9cca5346e79
soeren tietboehl <soeren.tietboehl@student.hpi.de>
2017-01-21 17:55:39 +0100

    change algo to use proper queue and set, add progressbar

--- related_artists.py
+++ related_artists.py
@@ -4,36 +4,40 @@ import json
 import requests
 import time
 import datetime
+import sys
+from collections import deque
+import progressbar
+
+maxsize = 20000 if len(sys.argv) == 1 else int(sys.argv[1])
 
 # list of artists to add
-pq = []
-all_artists = []
+pq = deque()
+all_artists = set()
 G = nx.Graph()
-artist_file = open('artist16.txt', 'wb')
 
 # example set upon Red Hot Chilli Peppers
 artist_id = u'0L8ExT028jH3ddEcZwqJJ5'
 pq.append(artist_id)
-all_artists.append(u'Red Hot Chili Peppers')
+all_artists.add(u'Red Hot Chili Peppers')
 count = 0
 
 st = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
 print(st)
 
+progressbar.draw(count, maxsize, 30)
 
-while len(pq) > count:
+while pq and count < maxsize:
 	first = False
-	currentArtist = pq[0]
-	del pq[0]
+	currentArtist = pq.pop()
 	r = requests.get('https://api.spotify.com/v1/artists/' + currentArtist + '/related-artists', auth = ())
 	
 	#print(r)
 	for i in r.json()['artists']:
-		if i['name'].encode('ascii','ignore') not in all_artists:
+		artist_name = i['name'].encode('ascii','ignore')
+		if artist_name not in all_artists:
 			#print(i['name'])
 			pq.append(i['id'])
-			all_artists.append(i['name'].encode('ascii','ignore'))
-			artist_file.write(i['name'].encode('ascii', 'ignore') + "\n")		
+			all_artists.add(artist_name)		
 			G.add_node(i['id'])
 		G.add_edge(currentArtist, i['id'])
 	
@@ -41,8 +45,8 @@ while len(pq) > count:
 		#if (count % 100) == 0:
 		#	nx.write_adjlist(G, 'graph' + str(count) + '.txt')
 	
-	# variable to cut the last items in priority queue, under 5 is time critical 
-	count += 4
+	count += 1
+	progressbar.redraw(count, maxsize, 30)
 #print(all_artists)
 st = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
 print(st)
@@ -52,5 +56,10 @@ print(len(all_artists))
 
 #file.write(json.dumps(H))
 #file.close()
-nx.write_adjlist(G, 'graph16.txt')
-artist_file.close()
+nx.write_adjlist(G, 'graph.txt')
+all_artists_sorted = list(all_artists)
+all_artists_sorted.sort()
+
+with open('artist.txt', 'wb') as artist_file:
+	for artist in all_artists_sorted:
+		artist_file.write(artist + "\n")
