b72eea5f3a01d82009b7b144e326008ac7c458b6
Maxi Fischer <maxi.fischer@student.hpi.de>
2017-01-24 01:59:59 +0100

    more late night coding

--- graph_analysis.py
+++ graph_analysis.py
@@ -1,16 +1,31 @@
 import networkx as nx
 import pylab as plt
+import matplotlib.pyplot as plt
 
-spotifygraph = nx.read_adjlist('graph.txt')
+spotifygraph = nx.read_adjlist('graph_report.txt')
 
-degrees = spotifygraph.degree() # dictionary node:degree
-values = sorted(set(degrees.values()))
-hist = [degrees.values().count(x) for x in values]
-plt.figure()
-plt.plot(values,hist,'ro-') # in-degree
-plt.legend(['In-degree','Out-degree'])
-plt.xlabel('Degree')
-plt.ylabel('Number of nodes')
-plt.title('Spotify related artist network')
-plt.savefig('spotify_degree_distribution.pdf')
-plt.close()
+# number of nodes and edges
+print(spotifygraph.number_of_nodes())
+print(spotifygraph.number_of_edges())
+
+# degree histogram
+# degrees = spotifygraph.degree() # dictionary node:degree
+# values = sorted(set(degrees.values()))
+# hist = [degrees.values().count(x) for x in values]
+# plt.figure()
+# plt.plot(values,hist,'ro-') # in-degree
+# plt.legend(['In-degree','Out-degree'])
+# plt.xlabel('Degree')
+# plt.ylabel('Number of nodes')
+# plt.title('Spotify related artist network')
+# plt.savefig('spotify_degree_distribution.pdf')
+# plt.close()
+
+# diameter
+#print(nx.diameter(spotifygraph))
+
+# triangles
+#print(sum(list(nx.triangles(spotifygraph).values())))
+
+# avg clustering coefficient
+print(nx.average_clustering(spotifygraph))
