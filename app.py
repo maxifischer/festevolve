6f516501f7117c8741b75130670c2a5ee0501a9c
Maxi Fischer <I341358@bnel50902416b.apj.global.corp.sap>
2017-06-07 09:46:54 +0300

    add init authorization

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
 import os
 import psycopg2
 import sys
+from os.path import expanduser
 
 from flask import Flask
 app = Flask(__name__)
+
+auth = open((expanduser('~') + '/Documents/festevolve/authorization/auth.txt'), 'r')
+auths = auth.read().split()
+client_id = auths[0]
+client_secret = auths[1]
+redirect_uri = auths[2]
+scopes = auths[3] + ' ' + auths[4]
+print(scopes)
 
 @app.route('/')
 def main():
 	# overview over all festivals
 	return "festevolve"
 
-@app.route('/helga/')
+@app.route('/search/')
 def helga():
 	# looking for helga
 	return 'HELGA!'
@@ -19,3 +28,6 @@ def helga():
 def show_festival(festival_id):
 	#show festival page with more information
 	return 'Festival %d', festival_id
+
+if __name__ == '__main__':
+	app.run(debug=True, host='0.0.0.0', port=8888)
